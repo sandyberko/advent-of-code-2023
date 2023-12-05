093f94c03bd10c491773607be84274fda100add3
Sandy Berkowitz <elybarak7589+github2@gmail.com>
2023-12-05 14:13:55 +0200

    day 5: part 2

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -4,6 +4,7 @@ const INPUT: &str = include_str!("input.txt");
 
 fn main() {
     println!("lowest location: {}", lowest_location(INPUT));
+    println!("lowest location ranged: {}", lowest_location_ranged(INPUT));
 }
 
 // const CATEGORIES: &[&str] = &[
@@ -16,7 +17,7 @@ fn main() {
 //     "location",
 // ];
 
-fn lowest_location(input: &str) -> u32 {
+fn lowest_location(input: &str) -> u64 {
     let mut sections = input.split("\n\n");
 
     let seeds = {
@@ -24,31 +25,14 @@ fn lowest_location(input: &str) -> u32 {
         let (_tag, seeds) = seeds_section.split_once(':').unwrap();
         seeds
             .split_whitespace()
-            .map(str::parse::<u32>)
+            .map(str::parse::<u64>)
             .collect::<Result<Vec<_>, _>>()
             .unwrap()
     };
 
     sections
         .fold(seeds, |prev, mapping| {
-            let mapping = mapping
-                .split('\n')
-                .skip(1)
-                .map(|sect_line| {
-                    let mut sect_line = sect_line
-                        .split_whitespace()
-                        .map(str::parse::<u32>)
-                        .map(Result::unwrap);
-
-                    let dest = sect_line.next().unwrap();
-                    let source = sect_line.next().unwrap();
-                    let length = sect_line.next().unwrap();
-
-                    assert!(sect_line.next().is_none());
-
-                    (source, (dest, length))
-                })
-                .collect::<BTreeMap<_, _>>();
+            let mapping = parse_mapping(mapping);
 
             let result = prev
                 .into_iter()
@@ -77,11 +61,113 @@ fn lowest_location(input: &str) -> u32 {
         .unwrap()
 }
 
+#[derive(PartialEq, Eq)]
+struct Range {
+    start: u64,
+    length: u64,
+}
+
+impl Ord for Range {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.start.cmp(&other.start)
+    }
+}
+
+impl PartialOrd for Range {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Range {
+    fn new(start: u64, length: u64) -> Self {
+        Self { start, length }
+    }
+
+    fn end(&self) -> u64 {
+        self.start + self.length
+    }
+}
+
+fn lowest_location_ranged(input: &str) -> u64 {
+    let mut sections = input.split("\n\n");
+
+    let mut seed_ranges = Vec::new();
+    {
+        let seeds_section = sections.next().unwrap();
+        let (_tag, seeds) = seeds_section.split_once(':').unwrap();
+        let mut seeds = seeds
+            .split_whitespace()
+            .map(str::parse::<u64>)
+            .map(Result::unwrap);
+
+        while let (Some(start), Some(length)) = (seeds.next(), seeds.next()) {
+            seed_ranges.push(Range::new(start, length));
+        }
+    };
+
+    sections
+        .fold(seed_ranges, |prev, mapping| {
+            let mapping = parse_mapping(mapping);
+
+            prev.into_iter()
+                .flat_map(|seed| {
+                    let mut cur_seed_end = seed.end();
+                    let mut result = Vec::new();
+                    for (&map_start, (dest, length)) in mapping
+                        .range(..=seed.end())
+                        .rev()
+                        .take_while(|(&map_start, (_, length))| seed.start < map_start + length)
+                    {
+                        let shift = *dest as i64 - map_start as i64;
+                        let map_end = map_start + length;
+
+                        if map_end < cur_seed_end {
+                            Range::new(map_end, cur_seed_end - map_end);
+                        }
+
+                        let start = map_start.max(seed.start);
+                        let length = cur_seed_end.min(map_end) - map_start.max(seed.start);
+                        result.push(Range::new(start.checked_add_signed(shift).unwrap(), length));
+                        cur_seed_end = start;
+                    }
+                    if cur_seed_end > seed.start {
+                        result.push(Range::new(seed.start, cur_seed_end - seed.start));
+                    }
+                    result
+                })
+                .collect()
+        })
+        .into_iter()
+        .min()
+        .unwrap()
+        .start
+}
+
+fn parse_mapping(mapping: &str) -> BTreeMap<u64, (u64, u64)> {
+    mapping
+        .split('\n')
+        .skip(1)
+        .map(|sect_line| {
+            let mut sect_line = sect_line
+                .split_whitespace()
+                .map(str::parse::<u64>)
+                .map(Result::unwrap);
+
+            let dest = sect_line.next().unwrap();
+            let source = sect_line.next().unwrap();
+            let length = sect_line.next().unwrap();
+
+            assert!(sect_line.next().is_none());
+
+            (source, (dest, length))
+        })
+        .collect::<BTreeMap<_, _>>()
+}
+
 #[cfg(test)]
 pub mod tests {
-    #[rustfmt::skip]
-    const INPUT: &str = 
-"seeds: 79 14 55 13
+    const INPUT: &str = r#"seeds: 79 14 55 13
 
 seed-to-soil map:
 50 98 2
@@ -113,11 +199,17 @@ temperature-to-humidity map:
 
 humidity-to-location map:
 60 56 37
-56 93 4";
+56 93 4"#;
 
     #[test]
     fn lowest_location() {
         let lowest_location = super::lowest_location(INPUT);
         assert_eq!(lowest_location, 35);
+    }
+
+    #[test]
+    fn lowest_location_ranged() {
+        let lowest_location = super::lowest_location_ranged(INPUT);
+        assert_eq!(lowest_location, 46);
     }
 }
