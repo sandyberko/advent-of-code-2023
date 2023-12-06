2f2a66903fe493fc67f07b5daa5a2cd7e79632bd
Sandy Berkowitz <elybarak7589+github2@gmail.com>
2023-12-06 18:09:00 +0200

    day 6: cleanup

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,12 +1,14 @@
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Product of winning strategy count: {}", winning_start_count(INPUT));
+    println!(
+        "Product of winning strategy count: {}",
+        winning_start_count(INPUT)
+    );
     //1108800
 
     println!("Single race: {}", single_race(INPUT));
     // 36919753
-
 }
 
 fn winning_start_count(input: &str) -> u64 {
@@ -36,35 +38,12 @@ fn single_race(input: &str) -> u64 {
 }
 
 fn winning_strategy_count(time: u64, best_distance: u64) -> u64 {
-    // eprintln!("Time: {time}, Best Distance: {best_distance}");
-
+    // t + sqrt(t^2 - 4 b) - ceil(t - sqrt(t^2 - 4 b)) + 1
     let b = f64::sqrt((time.pow(2) - 4 * (best_distance + 1)) as f64);
-    // x = 1/2 (time - sqrt(time^2 + 4 * best_dist))
-    let x_0 = ((time as f64 - b) / 2.0).ceil();
-    // x = 1/2 (time + sqrt(time^2 + 4 * best_dist))
-    let x_1 = ((time as f64 + b) / 2.0) + 1.0;
-    let result = x_1 - x_0;
-
-    // eprintln!("{x_1} - {x_0} = {result}");
-
-    // assert_eq!(result as u64, calc_manually(time, best_distance));
-
+    let x_0 = (time as f64 - b) / 2.0;
+    let x_1 = (time as f64 + b) / 2.0;
+    let result = x_1 - x_0.ceil() + 1.0;
     result as _
-}
-
-#[allow(dead_code)]
-fn calc_manually(time: u64, best_distance: u64) -> u64 {
-    let result = (0..time)
-        .filter(|hold_duration| {
-            let distance = hold_duration * (time - hold_duration);
-
-            eprintln!(" - Hold for {hold_duration}ms, distance {}", distance);
-
-            distance > best_distance
-        })
-        .count() as u64;
-    eprintln!("Result: {result}");
-    result
 }
 
 #[cfg(test)]
