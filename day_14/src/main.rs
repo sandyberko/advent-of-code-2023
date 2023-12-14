4f8210aa01a61a7973ede922d702592d8ba89585
Sandy Berkowitz <elybarak7589+github2@gmail.com>
2023-12-14 11:52:05 +0200

    day 14: part 2: working on it

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -1,9 +1,12 @@
+use std::mem;
+
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
     println!("Load: {}", load(INPUT));
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Pixel {
     Round,
     Cube,
@@ -41,6 +44,130 @@ fn load(input: &str) -> usize {
         .sum()
 }
 
+const CYCLE_COUNT: usize = 1_000_000_000;
+fn load_cycles(input: &str) -> usize {
+    let og_input = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    'O' => Pixel::Round,
+                    '#' => Pixel::Cube,
+                    '.' => Pixel::Empty,
+                    c => panic!("{c:?}"),
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut cycles = vec![og_input];
+
+    for i in 0..CYCLE_COUNT {
+        eprintln!("cycle {i}");
+        let mut input = cycles.last().unwrap().clone();
+        cycle(&mut input);
+
+        // eprintln!("After {} cycles:", i + 1);
+        // for row in &next_input {
+        //     for p in row {
+        //         match p {
+        //             Pixel::Round => eprint!("O"),
+        //             Pixel::Cube => eprint!("#"),
+        //             Pixel::Empty => eprint!("."),
+        //         }
+        //     }
+        //     eprintln!()
+        // }
+        // eprintln!();
+
+        if let Some(j) = cycles
+            .iter()
+            .enumerate()
+            .find_map(|(i, cycle)| (cycle == &input).then_some(i))
+        {
+            let period = i - j;
+            let remainder = CYCLE_COUNT % period;
+            let final_arrangement = &cycles[j + remainder];
+            let input_len = final_arrangement.len();
+            return final_arrangement
+                .iter()
+                .enumerate()
+                .map(|(y, row)| {
+                    row.iter()
+                        .map(|p| if *p == Pixel::Round { input_len - y } else { 0 })
+                        .sum::<usize>()
+                })
+                .sum();
+        } else {
+            cycles.push(input);
+        }
+    }
+
+    panic!("no periodicity")
+}
+
+fn cycle(input: &mut [Vec<Pixel>]) {
+    // north
+    for x in 0..input[0].len() {
+        let mut empty_span = 0;
+        for y in 0..input.len() {
+            match input[y][x] {
+                Pixel::Round => {
+                    input[y][x] = Pixel::Empty;
+                    input[y - empty_span][x] = Pixel::Round;
+                }
+                Pixel::Cube => empty_span = 0,
+                Pixel::Empty => empty_span += 1,
+            }
+        }
+    }
+
+    // west
+    for row in input.iter_mut() {
+        let mut empty_span = 0;
+        for x in 0..row.len() {
+            match row[x] {
+                Pixel::Round => {
+                    row[x] = Pixel::Empty;
+                    row[x - empty_span] = Pixel::Round;
+                }
+                Pixel::Cube => empty_span = 0,
+                Pixel::Empty => empty_span += 1,
+            }
+        }
+    }
+
+    // south
+    for x in 0..input[0].len() {
+        let mut empty_span = 0;
+        for y in (0..input.len()).rev() {
+            match input[y][x] {
+                Pixel::Round => {
+                    input[y][x] = Pixel::Empty;
+                    input[y + empty_span][x] = Pixel::Round;
+                }
+                Pixel::Cube => empty_span = 0,
+                Pixel::Empty => empty_span += 1,
+            }
+        }
+    }
+
+    // east
+    for row in input.iter_mut() {
+        let mut empty_span = 0;
+        for x in (0..row.len()).rev() {
+            match row[x] {
+                Pixel::Round => {
+                    row[x] = Pixel::Empty;
+                    row[x + empty_span] = Pixel::Round;
+                }
+                Pixel::Cube => empty_span = 0,
+                Pixel::Empty => empty_span += 1,
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     const INPUT: &str = concat! {
@@ -60,5 +187,11 @@ mod tests {
     fn load() {
         let load = super::load(INPUT);
         assert_eq!(load, 136);
+    }
+
+    #[test]
+    fn load_cycles() {
+        let load = super::load_cycles(INPUT);
+        assert_eq!(load, 64);
     }
 }
