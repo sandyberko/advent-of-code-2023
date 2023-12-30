e7ae1cf52be9d7a9cdf90b3771d920e31fd7a976
Sandy Berkowitz <elybarak7589+github2@gmail.com>
2023-12-30 17:07:52 +0200

    day 20: optimize ids

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -1,6 +1,10 @@
+use num::{integer::lcm, traits::Pow};
+use owo_colors::OwoColorize;
 use std::{
+    array,
     cell::Cell,
     collections::HashMap,
+    io::{self, Write},
     iter::Sum,
     ops::{Add, AddAssign},
 };
@@ -15,14 +19,27 @@ fn main() {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Module<'s> {
-    ty: ModuleType<'s>,
-    dest: Vec<&'s str>,
+    tag: &'s str,
+    ty: ModuleType,
+    dest: Vec<usize>,
+    send_period: [Cell<Option<usize>>; 2],
+}
+
+impl<'s> Module<'s> {
+    fn new(tag: &'s str, ty: ModuleType) -> Self {
+        Self {
+            tag,
+            ty,
+            dest: Vec::new(),
+            send_period: array::from_fn(|_| Cell::new(None)),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-enum ModuleType<'s> {
+enum ModuleType {
     Flip(Cell<Pulse>),
-    Conj(HashMap<&'s str, Cell<Pulse>>),
+    Conj(HashMap<usize, Cell<Pulse>>),
     Broadcaster,
 }
 
@@ -70,11 +87,12 @@ impl AddAssign for Pulses {
 const BROADCASTER: &str = "broadcaster";
 
 fn pulse_propogation(input: &str) -> usize {
-    let state = parse_modules(input);
+    let (state, ids) = parse_modules(input);
+    let initial_dest = [ids[BROADCASTER]];
 
     let pulses = (1..=1000)
         .map(|_i| {
-            let mut queue = vec![("button", Pulse::Low, &[BROADCASTER] as &[&str])];
+            let mut queue = vec![(usize::MAX, Pulse::Low, &initial_dest as &[_])];
             let mut pulses = Pulses { low: 0, high: 0 };
 
             while !queue.is_empty() {
@@ -89,7 +107,8 @@ fn pulse_propogation(input: &str) -> usize {
 
                         dest.iter().flat_map(move |dest| {
                             // eprintln!("{src} -{pulse:?}-> {dest}");
-                            self::pulse(state, src, dest, pulse)
+                            self::pulse(state, src, *dest, pulse)
+                                .map(|(pulse, dests)| (*dest, pulse, dests))
                         })
                     })
                     .collect();
@@ -102,74 +121,190 @@ fn pulse_propogation(input: &str) -> usize {
 }
 
 fn pulse_to_rx(input: &str) -> usize {
-    let state = parse_modules(input);
+    let (state, ids) = parse_modules(input);
+    let rx = ids["rx"];
+    let inital_dest = [ids[BROADCASTER]];
+
+    // receive_period(&state, Pulse::Low, "rx")
+
+    // let (mut flips, mut conjs) = (Vec::new(), Vec::new());
+    // for (tag, module) in &state {
+    //     match &module.ty {
+    //         ModuleType::Flip(_) => flips.push(tag),
+    //         ModuleType::Conj(_) => conjs.push(tag),
+    //         ModuleType::Broadcaster => (),
+    //     }
+    // }
 
     let mut presses = 0;
 
     loop {
         presses += 1;
-        let mut queue = vec![("button", Pulse::Low, &[BROADCASTER] as &[&str])];
+        let mut queue = vec![(usize::MAX, Pulse::Low, &inital_dest as &[usize])];
         while !queue.is_empty() {
-            let state = &state;
             let mut next_queue = Vec::new();
             for (src, pulse, dest) in queue {
                 for dest in dest.iter() {
                     // eprintln!("{src} -{pulse:?}-> {dest}");
-                    if pulse == Pulse::Low && *dest == "rx" {
+                    if pulse == Pulse::Low && *dest == rx {
                         return presses;
                     }
-                    if let Some(queued) = self::pulse(state, src, dest, pulse) {
-                        next_queue.push(queued);
+                    if let Some((pulse, dests)) = self::pulse(&state, src, *dest, pulse) {
+                        next_queue.push((*dest, pulse, dests));
                     }
                 }
             }
             queue = next_queue;
+            if presses % 2usize.pow(17) == 0 {
+                eprint!("\x1B[2J\x1B[1;1H{presses}");
+            }
         }
+
+        // eprint!("Press any key...");
+        // io::stderr().flush().unwrap();
+        // io::stdin().read_line(&mut String::new()).unwrap();
+        // eprint!("\x1B[2J\x1B[1;1H");
+
+        // for (i, tag) in flips.iter().enumerate() {
+        //     if let ModuleType::Flip(state) = &state.get(*tag).unwrap().ty {
+        //         if i > 0 && i % 10 == 0 {
+        //             eprintln!()
+        //         }
+
+        //         match state.get() {
+        //             Pulse::High => eprint!("{} ", tag.green()),
+        //             Pulse::Low => eprint!("{} ", tag.red()),
+        //         }
+        //     }
+        // }
+        // eprintln!();
+        // for tag in &conjs {
+        //     if let ModuleType::Conj(inputs) = &state.get(*tag).unwrap().ty {
+        //         eprint!("{tag}: [");
+        //         for (i, (input, state)) in inputs.iter().enumerate() {
+        //             if i > 0 {
+        //                 eprint!(" ");
+        //             }
+        //             match state.get() {
+        //                 Pulse::High => eprint!("{}", input.green()),
+        //                 Pulse::Low => eprint!("{}", input.red()),
+        //             }
+        //         }
+        //         eprintln!("]");
+        //     }
+        // }
+        // eprintln!("============");
     }
 }
 
-fn parse_modules(input: &str) -> HashMap<&str, Module<'_>> {
-    let mut state = input
+// fn receive_period(modules: &HashMap<&str, Module<'_>>, pulse: Pulse, target: &str) -> usize {
+//     eprintln!("receive {target} {pulse:?}");
+//     if let Some(period) = modules
+//         .iter()
+//         .filter_map(|(src_tag, src_module)| {
+//             if !src_module.dest.contains(&target) {
+//                 return None;
+//             }
+
+//             Some(send_period(modules, src_tag, pulse))
+//         })
+//         // FIXME that's not true?
+//         .min()
+//     {
+//         period
+//     } else {
+//         panic!("{target:?} will never receive a {pulse:?}")
+//     }
+// }
+
+// fn send_period(modules: &HashMap<&str, Module<'_>>, tag: &str, pulse: Pulse) -> usize {
+//     let module = modules.get(tag).unwrap();
+//     if let Some(period) = module.send_period[pulse as usize].get() {
+//         return period;
+//     }
+//     eprintln!("send {tag} {pulse:?}");
+//     let period = match &module.ty {
+//         ModuleType::Flip(_) => {
+//             receive_period(modules, Pulse::Low, tag)
+//                 * match pulse {
+//                     // the first pulse a flip receives, flips it to high and sends a high
+//                     Pulse::High => 1,
+//                     // to send a low, it neesd one more pulse
+//                     Pulse::Low => 2,
+//                 }
+//         }
+//         ModuleType::Conj(inputs) => match pulse {
+//             Pulse::High => inputs
+//                 .keys()
+//                 .map(|input| send_period(modules, input, Pulse::Low))
+//                 // FIXME that's not true either, it doesn't consider the case when high is sent but not all inputs are high
+//                 .min()
+//                 .expect("conj to have at least one input"),
+//             Pulse::Low => inputs
+//                 .keys()
+//                 .map(|input| send_period(modules, input, Pulse::High))
+//                 .fold(1, lcm),
+//         },
+//         ModuleType::Broadcaster => match pulse {
+//             Pulse::High => panic!("broadcaster never sends high"),
+//             Pulse::Low => 1,
+//         },
+//     };
+//     module.send_period[pulse as usize].set(Some(period));
+//     period
+// }
+
+fn parse_modules(input: &str) -> (Vec<Module<'_>>, HashMap<&'_ str, usize>) {
+    let mut ids = HashMap::new();
+    let mut next_id = 0usize;
+    let (mut modules, dest): (Vec<_>, Vec<_>) = input
         .lines()
         .map(|line| {
-            let (module, dest) = line.split_once("->").unwrap();
-            let dest = dest.split(',').map(str::trim).collect();
+            let (tag, dest) = line.split_once("->").unwrap();
+            let dest = dest.split(',').map(str::trim).collect::<Vec<_>>();
 
-            let module = module.trim();
-            let (module, ty) = if module == BROADCASTER {
-                (module, ModuleType::Broadcaster)
-            } else if let Some(module) = module.strip_prefix('%') {
-                (module, ModuleType::Flip(Cell::new(Pulse::Low)))
-            } else if let Some(module) = module.strip_prefix('&') {
-                (module, ModuleType::Conj(HashMap::new()))
+            let tag = tag.trim();
+            let (tag, ty) = if tag == BROADCASTER {
+                (tag, ModuleType::Broadcaster)
+            } else if let Some(tag) = tag.strip_prefix('%') {
+                (tag, ModuleType::Flip(Cell::new(Pulse::Low)))
+            } else if let Some(tag) = tag.strip_prefix('&') {
+                (tag, ModuleType::Conj(HashMap::new()))
             } else {
-                panic!("invalid module {module:?}")
+                panic!("invalid module {tag:?}")
             };
 
-            (module, Module { ty, dest })
-        })
-        .collect::<HashMap<_, _>>();
+            ids.insert(tag, next_id);
 
-    for (tag, module) in state.clone() {
-        for dest in module.dest {
-            if let Some(Module {
-                ty: ModuleType::Conj(inputs),
-                ..
-            }) = state.get_mut(dest)
-            {
-                inputs.insert(tag, Cell::new(Pulse::Low));
+            next_id += 1;
+
+            (Module::new(tag, ty), dest)
+        })
+        .unzip();
+
+    for (id, dest) in dest.into_iter().enumerate() {
+        for dest in dest {
+            let dest_id = ids.entry(dest).or_insert_with(|| {
+                let id = next_id;
+                modules.insert(id, Module::new(dest, ModuleType::Broadcaster));
+                next_id += 1;
+                id
+            });
+            modules[id].dest.push(*dest_id);
+            if let ModuleType::Conj(inputs) = &mut modules[*dest_id].ty {
+                inputs.insert(id, Cell::new(Pulse::Low));
             }
         }
     }
-    state
+    (modules, ids)
 }
 
-fn pulse<'m, 's>(
-    modules: &'m HashMap<&str, Module<'s>>,
-    src: &'s str,
-    dest: &'s str,
+fn pulse<'m>(
+    modules: &'m [Module<'_>],
+    src: usize,
+    dest: usize,
     pulse: Pulse,
-) -> Option<(&'s str, Pulse, &'m [&'s str])> {
+) -> Option<(Pulse, &'m [usize])> {
     let module = modules.get(dest)?;
     match &module.ty {
         ModuleType::Flip(state) => match pulse {
@@ -178,20 +313,20 @@ fn pulse<'m, 's>(
                     Pulse::High => Pulse::Low,
                     Pulse::Low => Pulse::High,
                 });
-                Some((dest, state.get(), &module.dest))
+                Some((state.get(), &module.dest))
             }
             Pulse::High => None,
         },
         ModuleType::Conj(inputs) => {
-            inputs.get(src).unwrap().set(pulse);
+            inputs[&src].set(pulse);
             let pulse = if inputs.values().all(|pulse| pulse.get() == Pulse::High) {
                 Pulse::Low
             } else {
                 Pulse::High
             };
-            Some((dest, pulse, &module.dest))
+            Some((pulse, &module.dest))
         }
-        ModuleType::Broadcaster => Some((dest, pulse, &module.dest)),
+        ModuleType::Broadcaster => Some((pulse, &module.dest)),
     }
 }
 
